8b4140166b5a420fcb0233d530a49751acce2e7a
kharada <kharada@atr.jp>
2011-03-15 17:52:07 +0900

    new file:   WorkingFileUtils/src/jp/atr/dni/bmi/desktop/workingfileutils/Bundle.properties 	new file:   WorkingFileUtils/src/jp/atr/dni/bmi/desktop/workingfileutils/NSCSVReader.java 	new file:   WorkingFileUtils/src/jp/atr/dni/bmi/desktop/workingfileutils/NSCSVWriter.java 	new file:   WorkingFileUtils/src/jp/atr/dni/bmi/desktop/workingfileutils/WorkingFileUtils.java 	new file:   WorkingFileUtils/src/jp/atr/dni/bmi/desktop/workingfileutils/layer.xml

---
+++ WorkingFileUtils/src/jp/atr/dni/bmi/desktop/workingfileutils/WorkingFileUtils.java
@@ -0,0 +1,60 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jp.atr.dni.bmi.desktop.workingfileutils;
+
+import java.io.File;
+import java.io.IOException;
+import jp.atr.dni.bmi.desktop.neuroshareutils.Entity;
+
+/**
+ *
+ * @author kharada
+ */
+public class WorkingFileUtils {
+
+    private final String WS_HOME = "." + File.separator + "workingfiles" + File.separator;
+    private String workingFilePath;
+
+    public WorkingFileUtils() {
+        this.workingFilePath = "";
+    }
+
+    public WorkingFileUtils(String workingFilePath) {
+        this.workingFilePath = workingFilePath;
+    }
+
+    public boolean createWorkingFileFromNeuroshare(String sourceFilePath, Entity entity) throws IOException {
+        String tmpFilePath = WS_HOME + System.currentTimeMillis() + "_" + hashCode() + ".csv";
+        File file = new File(tmpFilePath);
+
+        if (!file.createNewFile()) {
+            return false;
+        }
+
+        NSCSVWriter nsCsvWriter = new NSCSVWriter();
+        nsCsvWriter.createCSVFileFromNeuroshare(file.getAbsolutePath(), sourceFilePath, entity);
+
+        this.workingFilePath = file.getAbsolutePath();
+        return true;
+    }
+
+    public boolean removeWorkingFile() {
+        return new File(workingFilePath).delete();
+    }
+
+    /**
+     * @return the workingFilePath
+     */
+    public String getWorkingFilePath() {
+        return workingFilePath;
+    }
+
+    /**
+     * @param workingFilePath the workingFilePath to set
+     */
+    public void setWorkingFilePath(String workingFilePath) {
+        this.workingFilePath = workingFilePath;
+    }
+}
