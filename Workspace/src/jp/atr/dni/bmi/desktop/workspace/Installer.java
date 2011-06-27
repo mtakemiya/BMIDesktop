6b2c7f50d5bd872f79a6389d3584c99ae91b9986
kharada <kharada@atr.jp>
2011-06-27 17:18:08 +0900

    Add Installer.java to delete channels and workingfiles when User closed the application. Add removeAllChannels method. Remove extra workingfiles which already exist.

---
+++ Workspace/src/jp/atr/dni/bmi/desktop/workspace/Installer.java
@@ -0,0 +1,32 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jp.atr.dni.bmi.desktop.workspace;
+
+import jp.atr.dni.bmi.desktop.model.Workspace;
+import org.openide.modules.ModuleInstall;
+
+/**
+ * Manages a module's lifecycle. Remember that an installer is optional and
+ * often not needed at all.
+ */
+public class Installer extends ModuleInstall {
+
+    @Override
+    public void restored() {
+        // Note : This method will be called when User open or re-open the application. 
+        // ( Not open or re-open the Workspace window!!! Be Care! )
+        // Nothing to do.
+    }
+
+    @Override
+    public void close() {
+
+        // Note : This method will be called when User close the application. 
+        // ( Not close the Workspace window!!! Be Care! )
+
+        // Delete Channels and Working Files which exist on the workspace.
+        Workspace.removeAllChannels();
+    }
+}
