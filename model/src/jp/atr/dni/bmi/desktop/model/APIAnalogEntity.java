9982e30819b859478a8df61068cf1b7ee5f7d4b0
武宮誠 <takemiya@atr.jp>
2011-07-08 15:42:21 +0900

    added some files

---
+++ model/src/jp/atr/dni/bmi/desktop/model/APIAnalogEntity.java
@@ -0,0 +1,25 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jp.atr.dni.bmi.desktop.model;
+
+import jp.atr.dni.bmi.desktop.neuroshareutils.AnalogInfo;
+import jp.atr.dni.bmi.desktop.neuroshareutils.Entity;
+
+/**
+ *
+ * @author Makoto Takemiya - [武宮 誠] <br />
+ * <a href="http://www.atr.jp">ATR - [株式会社・国際電氣通信基礎技術研究所]</a>
+ *
+ * @version 2011/07/07
+ */
+public class APIAnalogEntity implements APIEntity {
+
+   private AnalogInfo nsnEntity;
+
+   @Override
+   public String getEntityLabel() {
+      return nsnEntity.getEntityInfo().getEntityLabel();
+   }
+}
